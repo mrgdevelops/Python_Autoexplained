755f0c14b1939962a14150989c378f0014b6ec20
mrgdevelops <mrgdevelops@gmail.com>
2024-11-03 11:00:58 +0100

    10 important Python concepts - initial commit

---
+++ 10_Important_Python_Concepts/10_Important_Python_Concepts.py
@@ -0,0 +1,5 @@
+# 10 Important Python Concepts in 20 minutes
+# https://www.youtube.com/watch?v=Gx5qb1uHss4
+
+print("10 Important Python Concepts in 20 minutes")
+print("A Python file shoud have .py extension")
